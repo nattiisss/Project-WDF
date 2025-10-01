5aa8639477cc9bcf73c3394f4d97f8323b3b33d8
nattiisss <buna24rr@student.ju.se>
2025-10-01 14:55:14 +0200

    table deleted

--- server.js
+++ server.js
@@ -15,42 +15,7 @@ app.get("/", function (req, res) {
   res.render("home", { name: "World", title: "Home" });
 });
 
-app.get("/main.css", (req, res) => {
-  res.sendFile(__dirname + "/public/css/main.css");
-});
-
 app.use(express.static("public"));
-
-app.get("/rawpersons", (req, res) => {
-  db.all("SELECT * FROM Person", (err, rawPersons) => {
-    if (err) {
-      console.log("Error: " + err);
-    } else {
-      console.log("Data found, sending back to the client...");
-      res.send(rawPersons);
-    }
-  });
-});
-
-app.get("/listpersons", (req, res) => {
-  db.all("SELECT * FROM Person", (err, rawPersons) => {
-    if (err) {
-      console.log("Error: " + err);
-    } else {
-      listPersonsHTML = "jerome<ul>";
-      rawPersons.forEach((onePerson) => {
-        listPersonsHTML += "<li>";
-        listPersonsHTML += `${onePerson.fname},`;
-        listPersonsHTML += `${onePerson.lname},`;
-        listPersonsHTML += `${onePerson.age},`;
-        listPersonsHTML += `${onePerson.email},`;
-        listPersonsHTML += "</li>";
-      });
-      listPersonsHTML += "</ul>";
-      res.send(listPersonsHTML);
-    }
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server up and running on ${port}...`);
