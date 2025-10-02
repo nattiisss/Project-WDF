5678d95bd4ce1f88786adad0bf464d9b1362d529
nattiisss <buna24rr@student.ju.se>
2025-10-02 16:43:30 +0200

    tables work

--- server.js
+++ server.js
@@ -6,16 +6,29 @@ const port = 5482;
 app.use(express.static("public"));
 
 const sqlite3 = require("sqlite3");
-const dbFile = "my-project-db.sqlite3.db";
-db = new sqlite3.Database(dbFile);
-// creates table Person at startup
+const dbFile = "my-projec-db.sqlite3";
+const db = new sqlite3.Database(dbFile, (err) => {
+  if (err) {
+    console.error("Could not open database:", err.message);
+  } else {
+    console.log("Connected to database:", dbFile);
+  }
+});
+
+app.get("/", (req, res) => {
+  db.all("SELECT * FROM Events", (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Database error");
+      return;
+    }
+    res.render("home", { events: rows, title: "Events list" });
+  });
+});
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
-app.get("/", function (req, res) {
-  res.render("home", { name: "World", title: "Home" });
-});
 
 app.listen(port, () => {
   console.log(`Server up and running on ${port}...`);
