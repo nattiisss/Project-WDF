e490a5f89a92cb90ee2f1922238b1d1d56262514
nattiisss <buna24rr@student.ju.se>
2025-10-20 14:46:19 +0200

    multer start

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "public/img/" });
 const app = express();
 const { engine } = require("express-handlebars");
 const port = 5482;
